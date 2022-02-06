9743d0cc919c4b3c409738cfd8e97dbb7b1b4e3a
WallyOxen <wally@wallys.world>
2022-02-05 21:50:39 -0500

    feat: Add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer app class="pt-4" color="grey lighten-3" mini-variant>
-      <v-avatar
-        v-for="n in 6"
-        :key="n"
-        :color="`grey ${n === 1 ? 'darken' : 'lighten'}-1`"
-        :size="n === 1 ? 36 : 20"
-        class="d-block text-center mx-auto mb-9"
-      ></v-avatar>
+    <v-navigation-drawer app class="pt-4" color="grey darken-3" mini-variant>
+      <router-link v-for="link in links" :key="link.nav" :to="link.nav">
+        <v-tooltip>
+          <template v-slot:activator="{ on, attrs }">
+            <v-avatar
+              v-on="on"
+              v-bind="attrs"
+              :color="
+                $router.currentRoute.path === link.nav ? link.color : 'grey'
+              "
+              :size="$router.currentRoute.path === link.nav ? 36 : 25"
+              class="d-block mx-auto mb-5"
+            >
+              <v-icon dark>{{ link.icon }}</v-icon>
+            </v-avatar>
+          </template>
+          <span>{{ link.name }}</span>
+        </v-tooltip>
+      </router-link>
     </v-navigation-drawer>
 
     <v-main>
@@ -19,5 +30,21 @@
 <script>
 export default {
   name: "SpaceXStats",
+  data: () => ({
+    links: [
+      {
+        name: "Latest Launches",
+        icon: "mdi-autorenew",
+        color: "orange",
+        nav: "/",
+      },
+      {
+        name: "Upcoming Launches",
+        icon: "mdi-clock-outline",
+        color: "green",
+        nav: "/upcoming",
+      },
+    ],
+  }),
 };
 </script>
